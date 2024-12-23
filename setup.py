6507284e037fd55716243c1354237590295c9e4a
bpph <mfeise_c21@kegs.org.uk>
2024-12-23 21:58:50 +0000

    new version ;-;

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kgreb',
-    version='v0.0.1',
+    version='v0.0.2',
     packages=['kgreb', 'kgreb.vle', 'kgreb.util'],
     url='https://github.com/BigPotatoPizzaHey/kgreb/',
     license='',
